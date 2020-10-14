0f427b4f9617cbad377bc5be4e7e065950c78fa3
Nikitosik3105 <72852668+Nikitosik3105@users.noreply.github.com>
2020-10-14 12:50:14 +0300

    Add files via upload

---
+++ Laba4(2).cpp
@@ -0,0 +1,60 @@
+// ConsoleApplication59.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "locale.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "windows.h"
+
+int pusirek(int *a, int n);
+
+int main(void)
+{
+	int i, n;
+	int *a;
+	setlocale(LC_ALL, "Russian");
+	printf("Введите размер массива:");
+	scanf_s("%d", &n);
+	a = (int*)malloc(n * sizeof(int));
+	if (a == NULL)
+	{
+		printf("Не достаточно памяти");
+		return 1;
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("a[%d]=", i);
+		scanf_s("%d", (a + i));
+	}
+	printf("Первоначальный массив:");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", *(a + i));
+	}
+	pusirek(a, n);
+	printf("\n");
+	printf("Изменненный массив:");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", *(a + i));
+	}
+	system("pause");
+	return 0;
+}
+
+int pusirek(int *a, int n)
+{
+	int e, d, tmp;
+	for (e = 0; e < n - 1; e++)
+		for (d = n - 1; d > e; d--)
+			if (*(a + d - 1) > *(a + d) && *(a + d) <= 0)
+			{
+				tmp = *(a + d - 1);
+				*(a + d - 1) = *(a + d);
+				*(a + d) = tmp;
+			}
+	return(a, n);
+}
+
